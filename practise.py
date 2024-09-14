d080c35253114d6d45bf414c1d3c4dbe4098ae15
Manish <manishsinghdewas@gmail.com>
2024-09-14 12:12:23 +0530

    for practise

---
+++ practise.py
@@ -0,0 +1,6 @@
+# Ankit BroCheck
+# Make list and and print it using while loop
+
+# Rupesh  BroCheck
+# create a input for name and email and password and confirm password for terminal login and pre store some data to for conformation of user login
+
